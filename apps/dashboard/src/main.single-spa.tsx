2cd5ac2161f4db36ebda9f7d3bab9fea2b85f89c
mandliyarajendra11 <mandliyarajendra0000@gmail.com>
2025-10-23 19:14:01 +0530

    reverted standalone

--- apps/dashboard/src/main.single-spa.tsx
+++ apps/dashboard/src/main.single-spa.tsx
@@ -3,12 +3,6 @@ import * as ReactDOMClient from 'react-dom/client';
 import singleSpaReact from 'single-spa-react';
 import App from './app/app'; 
 import { BrowserRouter } from 'react-router-dom';
-declare global {
-  interface Window {
-    singleSpaNavigate?: boolean;
-  }
-}
-
 const lifecycles = singleSpaReact({
   React,
   ReactDOMClient,
@@ -21,18 +15,6 @@ const lifecycles = singleSpaReact({
     return <div>Error in Dashboard MFE</div>;
   },
 });
-
-if (!window.singleSpaNavigate) {
-  const rootElement = document.getElementById('root');
-  if (rootElement) {
-    const root = ReactDOMClient.createRoot(rootElement);
-    root.render(
-      <BrowserRouter basename='/'>
-        <App />
-      </BrowserRouter>
-    );
-  }
-}
 
 export const bootstrap = lifecycles.bootstrap;
 export const mount = lifecycles.mount;
